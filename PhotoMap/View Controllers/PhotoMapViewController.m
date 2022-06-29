674b5554300f89d8f06a8cd0dce3121bfc87f8a7
Jocelyn Tseng <jocelyntseng@fb.com>
2022-06-29 09:39:15 -0700

    finished step 3 tagging a location

---
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -0,0 +1,82 @@
+//
+//  PhotoMapViewController.m
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "PhotoMapViewController.h"
+
+@interface PhotoMapViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *myMapView;
+@property (nonatomic, strong) UIImage *selectedImage;
+
+@end
+
+@implementation PhotoMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.myMapView setRegion:sfRegion animated:false];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)locationsViewController:(LocationsViewController *)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude {
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)didTapCamera:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+//    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.selectedImage = editedImage;
+    
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    [self performSegueWithIdentifier:@"tagSegue" sender:nil];
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    if([segue.identifier isEqualToString:@"tagSegue"]) {
+        LocationsViewController *vc = segue.destinationViewController;
+        vc.delegate = self;
+    }
+}
+
+@end
